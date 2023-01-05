cbbeaf7c94f0151cc231d242569f0c939ab6657e
alvrdz2002 <112970056+alvrdz2002@users.noreply.github.com>
2023-01-04 18:01:54 -0600

    Add files via upload

---
+++ 01_OOP/E1/Transport/Transport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioManager
+{
+    public class Transport
+    {
+
+        public virtual void TransportSound()
+        {
+            Console.WriteLine("I´m a Transport");
+        }
+
+    }
+}
