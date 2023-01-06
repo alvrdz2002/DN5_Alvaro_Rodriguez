61a7d38054ad15e1dbed49bbd1c16e2062f85327
alvrdz2002 <112970056+alvrdz2002@users.noreply.github.com>
2023-01-06 15:47:30 -0600

    EXAMPLE
    UPDATE

---
+++ 02_BOOTSTRAP/E1/Transport/Train.cs
@@ -0,0 +1,29 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioManager
+{
+    public class Train : Transport
+    {
+        public override void TransportSound()
+        {
+
+            AudioFileReader audioFile = new AudioFileReader("Resources/train.mp3");
+            WaveOutEvent waveOutEvent = new WaveOutEvent();
+
+            waveOutEvent.Init(audioFile);
+            waveOutEvent.Play();
+            while (waveOutEvent.PlaybackState == PlaybackState.Playing)
+            {
+                System.Threading.Thread.Sleep(500);
+            }
+
+            waveOutEvent.Dispose();
+            audioFile.Dispose();
+        }
+    }
+}
