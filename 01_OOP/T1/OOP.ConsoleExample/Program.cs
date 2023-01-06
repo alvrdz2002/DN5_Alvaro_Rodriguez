5eac3ec4c069d8a2bf31527336645d61a437f0c3
alvrdz2002 <112970056+alvrdz2002@users.noreply.github.com>
2023-01-05 21:20:32 -0600

    J
    K

---
+++ 01_OOP/T1/OOP.ConsoleExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioManager;
+
+
+namespace OOP.ConsoleExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            Animal animal = new Animal();
+            animal.AnimalSound();
+
+            Console.ReadKey();
+        }
+    }
+}
