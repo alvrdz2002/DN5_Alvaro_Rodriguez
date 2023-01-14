8c8a2dd915d22a4c3dcdbb9bf7534631640423c3
alvrdz2002 <112970056+alvrdz2002@users.noreply.github.com>
2023-01-14 14:28:06 -0600

    T4
    still incomplete

---
+++ 02_WebDeveloment/T4/DataTypes/DataTypes.Web/wwwroot/dataTypes.js
@@ -0,0 +1,6 @@
+﻿
+var element = document.getElementById("example");
+var textExample = "this is a text";
+var aNumber = 10.333;
+
+element.innerHTML = typeof (aNumber);
---
+++ 02_WebDeveloment/T4/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+
+namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = "This a string";
+            int age = 35;
+            DateTime now = DateTime.Now;
+
+            double amount = 0;
+
+            string string2 = text + " a second string " + age + now + amount;
+
+            string string3 = string.Format("The age is {0}, the time is {1}, and te amount is {2}", age, now, amount);
+
+            string string4 = $"The age is {age}, the time is {now}, and te amount is {amount}";
+
+            char sampleChar = 'C';
+            char[] arrayChar = string4.ToCharArray();
+
+            amount = (double)10 / (double)3;
+
+            DateTime dateTime = new DateTime(2000, 1, 1);
+
+            var timeStamp1 = now - dateTime;
+            //var otherText = "this is a text";
+
+            Console.WriteLine(timeStamp1.TotalMilliseconds);
+
+            Console.ReadKey();
+
+        }
+    }
+}
