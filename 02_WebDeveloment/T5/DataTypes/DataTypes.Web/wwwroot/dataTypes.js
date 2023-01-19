0d849e0fa8e5a151f639abe0100a3007ac55c97f
alvrdz2002 <112970056+alvrdz2002@users.noreply.github.com>
2023-01-18 18:08:22 -0600

    T6
    Complete

---
+++ 02_WebDeveloment/T5/DataTypes/DataTypes.Web/wwwroot/dataTypes.js
@@ -0,0 +1,53 @@
+﻿
+function onButtonClick(evt) {
+    /////////////
+    var b = evt.currentTarget;
+
+    var classes = b.classList;
+
+    var isGreen = false;
+
+    for (var i = 0; i < classes.length; i++) {
+
+        var c = classes[i];
+
+        if (c == "greenButton") {
+            isGreen = true;
+            break;
+        }
+    }
+
+    if (isGreen) {
+
+        b.className = "btn redButton";
+
+    } else {
+
+        b.className = "btn greenButton";
+
+    }
+
+    var t = document.getElementById("sampleTable");
+
+    var currentHtml = t.innerHTML;
+
+    t.innerHTML += "<tr><td>" + b.getAttribute("name") + "</td><td>" + new Date() + "</td><tr/>";
+
+
+
+    ////////////
+}
+
+//var button = document.getElementsByTagName("button")[0];
+var buttons = document.getElementsByTagName("button");
+
+for (var i = 0; i < buttons.length; i++) {
+
+    buttons[i].addEventListener("click", onButtonClick);
+
+}
+
+debugger;
+
+
+//button.addEventListener("click", onButtonClick);
