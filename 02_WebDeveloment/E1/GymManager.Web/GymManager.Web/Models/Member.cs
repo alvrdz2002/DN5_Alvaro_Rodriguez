025ee647b5f2e0d6db320c07a3370b25b2a2b3e1
alvrdz2002 <112970056+alvrdz2002@users.noreply.github.com>
2023-01-20 15:06:09 -0600

    E2 and E3
    Incomplete

---
+++ 02_WebDeveloment/E1/GymManager.Web/GymManager.Web/Models/Member.cs
@@ -0,0 +1,19 @@
+﻿namespace GymManager.Web.Models
+{
+    public class Member
+    {
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Birthday { get; set; }
+
+        public int CityId { get; set; }
+
+        public string Email { get; set; }
+
+        public bool AllowNewsletter  { get; set; }
+
+    }
+}
